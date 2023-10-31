49c9e485ff022013198f16bee177659233c8a5a8
Maunik <112779591+vaghani04@users.noreply.github.com>
2023-10-31 23:11:18 +0530

    Add files via upload

--- helper_functions.py
+++ helper_functions.py
@@ -1,23 +1,128 @@
+import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
-def plot_time_series(timesteps, values, format='.', start=0, end=None, label=None):
-  """
-  Plots a timesteps (a series of points in time) against values (a series of values across timesteps).
 
-  Parameters
-  ---------
-  timesteps : array of timesteps
-  values : array of values across time
-  format : style of plot, default "."
-  start : where to start the plot (setting a value will index from start of timesteps & values)
-  end : where to end the plot (setting a value will index from end of timesteps & values)
-  label : label to show on plot of values
+def plot_time_series(timesteps, values, format='.', start=0, end=None, label=None):
+    """
+    Plots a timesteps (a series of points in time) against values (a series of values across timesteps).
+
+    Parameters
+    ---------
+    timesteps : array of timesteps
+    values : array of values across time
+    format : style of plot, default "."
+    start : where to start the plot (setting a value will index from start of timesteps & values)
+    end : where to end the plot (setting a value will index from end of timesteps & values)
+    label : label to show on plot of values
+    """
+    # Plot the series
+    plt.plot(timesteps[start:end], values[start:end], format, label=label)
+    plt.xlabel("Time")
+    plt.ylabel("BTC Price")
+    if label:
+        plt.legend(fontsize=14)  # make label bigger
+    plt.grid(True)
+
+
+def mean_absolute_scaled_error(y_true, y_pred):
+    """
+    Implement MASE (assuming no seasonality of data).
+    """
+    mae = tf.reduce_mean(tf.abs(y_true - y_pred))
+
+    # Find MAE of naive forecast (no seasonality)
+    # our seasonality is 1 day (hence the shifting of 1 day)
+    mae_naive_no_season = tf.reduce_mean(tf.abs(y_true[1:] - y_true[:-1]))
+
+    return mae / mae_naive_no_season
+
+
+def evaluate_preds(y_true, y_pred):
+  # Make sure float32 (for metric calculations)
+  y_true = tf.cast(y_true, dtype=tf.float32)
+  y_pred = tf.cast(y_pred, dtype=tf.float32)
+
+  # Calculate various metrics
+  mae = tf.keras.metrics.mean_absolute_error(y_true, y_pred)
+  mse = tf.keras.metrics.mean_squared_error(y_true, y_pred)
+  rmse = tf.sqrt(mse)
+  mape = tf.keras.metrics.mean_absolute_percentage_error(y_true, y_pred)
+  mase = mean_absolute_scaled_error(y_true, y_pred)
+
+  # Account for different sized metrics (for longer horizons, reduce to single number)
+  if mae.ndim > 0: # if mae isn't already a scalar, reduce it to one by aggregating tensors to mean
+    mae = tf.reduce_mean(mae)
+    mse = tf.reduce_mean(mse)
+    rmse = tf.reduce_mean(rmse)
+    mape = tf.reduce_mean(mape)
+    mase = tf.reduce_mean(mase)
+
+  return {"mae": mae.numpy(),
+          "mse": mse.numpy(),
+          "rmse": rmse.numpy(),
+          "mape": mape.numpy(),
+          "mase": mase.numpy()}
+
+
+
+def get_labelled_windows(x, horizon=1):
+    """
+    Creates labels for windowed dataset.
+
+    E.g. if horizon=1
+    Input: [0, 1, 2, 3, 4, 5, 6, 7] -> Output: ([0, 1, 2, 3, 4, 5, 6], [7])
+    """
+    return x[:, :-horizon], x[:, -horizon:]
+
+
+# Create function to view NumPy arrays as windows
+
+
+def make_windows(x, window_size=7, horizon=1):
+    """
+    Turns a 1D array into a 2D array of sequential windows of window_size.
+    """
+    # 1. Create a window of specific window_size (add the horizon on the end for later labelling)
+    window_step = np.expand_dims(np.arange(window_size+horizon), axis=0)
+    print(f"Window step:\n {window_step}")
+
+    # 2. Create a 2D array of multiple window steps (minus 1 to account for 0 indexing)
+    # create 2D array of windows of size window_size
+    window_indexes = window_step + \
+        np.expand_dims(np.arange(len(x)-(window_size+horizon-1)), axis=0).T
+    print(
+        f"Window indexes:\n {window_indexes[:3], window_indexes[-3:], window_indexes.shape}")
+
+    # 3. Index on the target array (time series) with 2D array of multiple window steps
+    windowed_array = x[window_indexes]
+
+    # 4. Get the labelled windows
+    windows, labels = get_labelled_windows(windowed_array, horizon=horizon)
+
+    return windows, labels
+
+
+# Make the train/test splits
+
+def make_train_test_splits(windows, labels, test_split=0.2):
+    """
+    Splits matching pairs of windows and labels into train and test splits.
+    """
+    split_size = int(len(windows) * (1-test_split)
+                     )  # this will default to 80% train/20% test
+    train_windows = windows[:split_size]
+    train_labels = labels[:split_size]
+    test_windows = windows[split_size:]
+    test_labels = labels[split_size:]
+    return train_windows, test_windows, train_labels, test_labels
+
+
+def make_preds(model, input_data):
   """
-  # Plot the series
-  plt.plot(timesteps[start:end], values[start:end], format, label=label)
-  plt.xlabel("Time")
-  plt.ylabel("BTC Price")
-  if label:
-    plt.legend(fontsize=14) # make label bigger
-  plt.grid(True)
+  Uses model to make predictions input_data.
+  """
+  forecast = model.predict(input_data)
+  return tf.squeeze(forecast) # return 1D array of predictions
+
+
