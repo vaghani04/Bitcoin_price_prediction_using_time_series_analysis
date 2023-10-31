7902000059a534df7955c00a5f8b489021975ad7
Maunik <112779591+vaghani04@users.noreply.github.com>
2023-10-31 21:44:52 +0530

    Add files via upload

---
+++ helper_functions.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+def plot_time_series(timesteps, values, format='.', start=0, end=None, label=None):
+  """
+  Plots a timesteps (a series of points in time) against values (a series of values across timesteps).
+
+  Parameters
+  ---------
+  timesteps : array of timesteps
+  values : array of values across time
+  format : style of plot, default "."
+  start : where to start the plot (setting a value will index from start of timesteps & values)
+  end : where to end the plot (setting a value will index from end of timesteps & values)
+  label : label to show on plot of values
+  """
+  # Plot the series
+  plt.plot(timesteps[start:end], values[start:end], format, label=label)
+  plt.xlabel("Time")
+  plt.ylabel("BTC Price")
+  if label:
+    plt.legend(fontsize=14) # make label bigger
+  plt.grid(True)
